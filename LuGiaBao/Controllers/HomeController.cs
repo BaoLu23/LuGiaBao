0902b2ee942f400ffaf2c27a428abd78e82a50fa
BaoLu23 <baoblbc111@gmail.com>
2022-06-21 15:55:49 +0700

    Add project files.

---
+++ LuGiaBao/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using LuGiaBao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuGiaBao.Controllers
+{
+    public class HomeController : Controller
+    {
+        Model1 db = new Model1();
+        public ActionResult Index()
+        {
+            var all_phim = from s in db.Phims select s;
+            return View(all_phim);
+        }
+
+        public ActionResult LichChieu()
+        {
+            var LichChieus = from s in db.LichChieux select s;
+
+            return View(LichChieus);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LuGiaBao/Models/Giohang.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LuGiaBao.Models
+{
+    public class Giohang
+    {
+        Model1 db = new Model1();
+        public int id { get; set; }
+        [Display(Name = "Mã lịch chiếu")] public string malich { get; set; }
+        [Display(Name = "Mã phim")] public string maphim { get; set; }
+        [Display(Name = "Tên phim")] public string tenphim { get; set; }
+
+        [Display(Name = "Giá")] public Double gia { get; set; }
+        [Display(Name = "Số lượng")] public int iSoluong { get; set; }
+        [Display(Name = "Thành tiền")] 
+        public Double dThanhtien
+        {
+            get { return iSoluong * gia; }
+        }
+       public Giohang(string id)
+        {
+            malich = id;
+            LichChieu lich = db.LichChieux.Single(n => n.malich == malich);
+            tenphim = lich.Phim.tenphim;
+            gia = double.Parse(lich.gia.ToString());
+            iSoluong = 1;
+        }
+
+        public Giohang(int id)
+        {
+            this.id = id;
+        }
+    }
+}
